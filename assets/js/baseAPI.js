eccc014dd55819e68d00dd1053c8e08bdb57e9f5
silvaer <1065531115@qq.com>
2021-03-13 15:04:45 +0800

    登录注册功能完成

---
+++ assets/js/baseAPI.js
@@ -0,0 +1,13 @@
+
+// 1.开发环境路径
+let baseURL = 'http://api-breakingnews-web.itheima.net';
+// 2.测试环境路径
+// let baseURL = 'http://api-breakingnews-web.itheima.net';
+// 3.生产环境路径
+// let baseURL = 'http://api-breakingnews-web.itheima.net';
+
+
+$.ajaxPrefilter(function (options) {
+    console.log(options);
+    options.url = baseURL + options.url;
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,55 @@ $(function () {
         $('.reg-box').hide();
         $('.login-box').show();
     })
+
+    let form = layui.form;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/,
+            '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            let pwd = $('.reg-box input[name="password"]').val();
+            if (value != pwd) return "两次密码输入不一致!";
+        }
+    })
+
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/api/reguser",
+            data: {
+                username: $('.reg-box input[name="username"]').val(),
+                password: $('.reg-box input[name="password"]').val()
+            },
+            success: (res) => {
+                console.log(res);
+                if (res.status != 0)
+                    return layer.msg(res.message, { icon: 5 });
+                layer.msg("恭喜您，注册成功", { icon: 6 }, function () {
+                    $('#link_login').click();
+                    $('#form_reg')[0].reset();
+                });
+            }
+        })
+    })
+
+
+    $('#form_login').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "/api/login",
+            data: $(this).serialize(),
+            success: (res) => {
+                if (res.status != 0) return layer.msg(res.message, { icon: 5 })
+                layer.msg('恭喜您，登录成功', { icon: 6 });
+                localStorage.getItem('myToken', res.token);
+                location.href = '/index.html';
+                // location.assign('/index.html');
+            }
+        })
+    })
+
 })
