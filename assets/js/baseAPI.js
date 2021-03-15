23a90e4b4c903f274fc4456fa90572f2fac901cf
silvaer <1065531115@qq.com>
2021-03-15 09:58:46 +0800

    完成首页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -10,4 +10,20 @@ let baseURL = 'http://api-breakingnews-web.itheima.net';
 $.ajaxPrefilter(function (options) {
     // console.log(options);
     options.url = baseURL + options.url;
+
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('myToken') || ''
+        }
+
+        options.complete = function (res) {
+            // console.log(res);
+            let obj = res.responseJSON;
+            // console.log(obj);
+            if (obj.status && obj.message === "身份认证失败！") {
+                localStorage.removeItem('myToken');
+                location.href = '/login.html';
+            }
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,40 @@
 $(function () {
     getUserinfo();
+
+    $('#btnlogout').click(function () {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('myToken');
+            location.href = '/login.html';
+
+            layer.close(index);
+        });
+    })
 })
 function getUserinfo() {
     $.ajax({
         type: "get",
         url: "/my/userinfo",
-        Headers: {
-            Authorization: localStorage.getItem('myToken')
-        },
         data: {},
         success: (res) => {
             console.log(res);
+            if (res.status != 0) {
+                return layer.msg(res.message, { icon: 6 });
+            }
+            renderAvatar(res.data);
         }
     })
+}
+
+
+function renderAvatar(user) {
+    let name = user.nickname || user.username;
+    $('#welcome').text(`欢迎` + name);
+    if (user.user_pic == null) {
+        $('.layui-nav-img').hide();
+        $('.text_avater').show().text(name[0].toUpperCase());
+    } else {
+        $(".layui-nav-img").show().attr("src", user.user_pic);
+        $('.text_avater').hide();
+    }
 }
